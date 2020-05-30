fa4390b1d48b8b11c4b674e247a208e27e3b34ff
ejimm2015 <moh.lam9596@gmail.com>
2020-05-30 18:40:30 +0200

    adding HealthCheck endpoint

---
+++ src/main/java/fr/dauphine/microservice/reader/api/HealthCheck.java
@@ -0,0 +1,16 @@
+package fr.dauphine.microservice.reader.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/healthcheck")
+public class HealthCheck {
+
+        @GetMapping
+        public ResponseEntity<String> healthcheck(){
+            return ResponseEntity.ok("Knock knock Neo, The Matrix has you ...");
+        }
+}
